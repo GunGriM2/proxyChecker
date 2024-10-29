2221b3cd274cf781d62d807a234d3c17b0b3253f
Максим <maks.grin.2016@gmail.com>
2024-10-29 10:29:19 +0300

    init

---
+++ app/Http/Controllers/ProxyCheckController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProxyCheckController extends Controller
+{
+    public function check(Request $request) {
+
+    }
+}
